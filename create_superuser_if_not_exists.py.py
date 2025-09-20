bc01f095cd995062b96ffe43fa02875a4578f1ed
GustavoValdesZ <gustavovaldes.z@gmail.com>
2025-09-20 19:43:53 -0300

    Agrega script para crear superuser automáticamente

---
+++ create_superuser_if_not_exists.py.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ElTotem.settings')
+django.setup()
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='TuPasswordSegura123'
+    )
+    print("Superuser creado")
+else:
+    print("Superuser ya existe")
