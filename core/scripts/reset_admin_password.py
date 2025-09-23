da6084aee4f05801bd3df4ceabc2f39e3fe241ab
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 18:34:08 -0300

    Update reset_admin_password.py

--- core/scripts/reset_admin_password.py
+++ core/scripts/reset_admin_password.py
@@ -1,9 +1,14 @@
 # core/scripts/reset_admin_password.py
+import sys
 import os
 import django
 
+# Agregamos el path raíz del proyecto
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ElTotem.settings')
 django.setup()
+
 
 from django.contrib.auth.models import User
 
