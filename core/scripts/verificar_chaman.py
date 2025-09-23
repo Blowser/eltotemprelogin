99b4e7c739a8f654755ab9098fb71cb5489740b5
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 17:10:38 -0300

    Update verificar_chaman.py
    más verificación

--- core/scripts/verificar_chaman.py
+++ core/scripts/verificar_chaman.py
@@ -12,3 +12,24 @@ if user:
     print(f"✅ Chamán encontrado: {user.username}")
 else:
     print("❌ El chamán no responde. Verificá la contraseña o existencia.")
+    
+
+from django.contrib.auth.models import User
+from core.models import Usuario
+
+user = User.objects.get(username='admin')
+perfil = Usuario.objects.filter(user=user).first()
+
+if perfil:
+    print("✅ El chamán tiene perfil")
+else:
+    print("❌ El chamán no tiene perfil. Creando...")
+
+    Usuario.objects.create(
+        user=user,
+        nombre='Admin',
+        apellido='Totémico',
+        rol_id=1  # Ajustá según tus roles disponibles
+    )
+    print("✅ Perfil creado con éxito")
+
