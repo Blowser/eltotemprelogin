154b91ba26067dd55c64bfc2066640789f4841b8
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 15:14:24 -0300

    Update wsgi.py
    VERSIÓN LIGERA

--- ElTotem/wsgi.py
+++ ElTotem/wsgi.py
@@ -14,19 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ElTotem.settings')
 
 application = get_wsgi_application()
-# 🔥 Invocar al chamán si no existe
+
+
+# Verificar superuser creado
+# 🔍 Verificar superuser creado
 try:
     from django.contrib.auth import get_user_model
     User = get_user_model()
 
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='admin',
-            email='admin@example.com',
-            password='TuPasswordSegura123'
-        )
-        print("🔥 Superusuario creado desde wsgi")
-    else:
-        print("🧙‍♂️ Superusuario ya existe")
+    for u in User.objects.all():
+        print(f"👤 Usuario: {u.username} | Staff: {u.is_staff} | Superuser: {u.is_superuser}")
 except Exception as e:
-    print(f"⚠️ Error creando superusuario: {e}")
+    print(f"⚠️ Error al listar usuarios: {e}")
