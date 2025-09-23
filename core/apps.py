713b3d028abedea08e1ca3d8c7dbe741878b6e69
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 13:47:06 -0300

    Update apps.py
    SE VUELVE A APP.PY DEFAULT

--- core/apps.py
+++ core/apps.py
@@ -4,8 +4,3 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-    def ready(self):
-        from .models import Rol
-        # Crear roles iniciales si no existen
-        for rol_id, tipo in Rol.ROLES_INICIALES:
-            Rol.objects.get_or_create(id_rol=rol_id, defaults={'tipo_rol': tipo})
