dc802535fb7168e1f6d2c173e77f1a8ff1c821d7
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 19:16:34 -0300

    Update settings.py

--- ElTotem/settings.py
+++ ElTotem/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY y DEBUG vía variables de entorno (o fallback)
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-rbz-bj-ux1w9wpz7%#d-9r)ni-6709j&7is)+0hq*kyw=c0ls5')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 
 
 # Dominios permitidos
