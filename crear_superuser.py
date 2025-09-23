d4eb262008eb22a3092c576080278e0a04c74f8e
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 17:20:25 -0300

    Create crear_superuser.py

---
+++ crear_superuser.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from core.models import Usuario
+
+class Command(BaseCommand):
+    help = 'Crea un superuser y su perfil totémico'
+
+    def handle(self, *args, **kwargs):
+        username = 'admin'
+        password = 'eltotem123'
+        email = 'admin@example.com'
+
+        user, created = User.objects.get_or_create(username=username)
+        if created:
+            user.email = email
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
+            self.stdout.write("✅ Chamán creado")
+
+        else:
+            user.set_password(password)
+            user.save()
+            self.stdout.write("🔄 Chamán ya existía, contraseña actualizada")
+
+        if not hasattr(user, 'perfil'):
+            Usuario.objects.create(
+                user=user,
+                nombre='Admin',
+                apellido='Totémico',
+                rol_id=1  # Asegurate de que exista el rol con ID 1
+            )
+            self.stdout.write("✅ Perfil totémico creado")
+        else:
+            self.stdout.write("🔍 El chamán ya tenía perfil")
