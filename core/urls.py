268862bfd9f59d7adf80561a389227ea3e68111d
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-23 12:15:36 -0300

    Update urls.py
    ARCHIVOS DE CONFIGURACIÓN

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views
-
+from .views import ProductosView, AccesoriosView
 urlpatterns = [
     path('', views.index, name='index'),
     path('registrarse/', views.registrarse_view, name='registrarse'),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('quienes-somos/', views.quienes_somos_view, name='quienes_somos'),
     path('noticias/', views.NoticiasFiltradasView.as_view(), name='noticias'),
     path('scrap-tcg/', views.scrap_tcg, name='scrap_tcg'),
+    path('productos/', ProductosView, name='productos'),
+    path('producto/<int:id>/', views.detalle_producto, name='detalle_producto'),
+    path('accesorios/', AccesoriosView, name='accesorios'),
 ]
