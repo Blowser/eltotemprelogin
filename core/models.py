ff50ab246a2df8ca1d339a112508a808c1463a07
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-26 16:48:57 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -204,7 +204,8 @@ class Thread(models.Model):
     titulo = models.CharField(max_length=20)
     asunto = models.CharField(max_length=200)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
+
     
     def __str__(self):
         return f"Hilo: {self.titulo}"
