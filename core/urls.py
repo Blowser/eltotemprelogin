f2d6b8906e9d656426770d1414c53552dd7eb990
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-25 21:42:38 -0300

    Update urls.py
    SE ACTIVA LOGOUT

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,14 @@
 from django.urls import path
 from . import views
 from .views import ProductosView, AccesoriosView
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('registrarse/', views.registrarse_view, name='registrarse'),
     path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('perfil/', views.ver_perfil, name='ver_perfil'),
     path('quienes-somos/', views.quienes_somos_view, name='quienes_somos'),
     path('noticias/', views.NoticiasFiltradasView.as_view(), name='noticias'),
@@ -14,4 +18,5 @@ urlpatterns = [
     path('productos/', ProductosView, name='productos'),
     path('producto/<int:id>/', views.detalle_producto, name='detalle_producto'),
     path('accesorios/', AccesoriosView, name='accesorios'),
+    
 ]
