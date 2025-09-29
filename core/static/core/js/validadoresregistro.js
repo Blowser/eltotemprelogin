619d2a97047f077d02f65758447464e584011fa4
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-28 21:11:42 -0300

    Update validadoresregistro.js

--- core/static/core/js/validadoresregistro.js
+++ core/static/core/js/validadoresregistro.js
@@ -1,4 +1,3 @@
-// core/static/core/js/validadoresregistro.js
 console.log('Validador Totémico activo');
 
 (function($) {
@@ -7,20 +6,21 @@ console.log('Validador Totémico activo');
       e.preventDefault();
 
       // Lectura de campos
-      const username  = $('#username').val().trim();
-      const nombre    = $('#nombre').val().trim();
-      const apellido  = $('#apellido').val().trim();
-      const email     = $('#email').val().trim();
-      const pw1       = $('#password1').val();
-      const pw2       = $('#password2').val();
-      const direccion = $('#direccion').val().trim();
-      const comuna    = $('#comuna').val().trim();
-      const region    = $('#region').val().trim();
+      const username          = $('#username').val().trim();
+      const nombre            = $('#nombre').val().trim();
+      const apellido          = $('#apellido').val().trim();
+      const email             = $('#email').val().trim();
+      const pw1               = $('#password1').val();
+      const pw2               = $('#password2').val();
+      const direccion         = $('#direccion').val().trim();
+      const numero_dpto_casa  = $('#numero_dpto_casa').val().trim();
+      const comuna            = $('#comuna').val().trim();
+      const region            = $('#region').val().trim();
 
       // Validación
       const errors = {};
-      const reLetters   = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{3,30}$/; // incluye espacios y tildes
-      const reUsername  = /^[A-Za-z0-9_]{3,30}$/;           // letras, números y guion bajo
+      const reLetters   = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{3,30}$/;
+      const reUsername  = /^[A-Za-z0-9_]{3,30}$/;
       const reEmail     = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       const rePw        = /^(?=.*\d)(?=.*[A-Z]).{6,18}$/;
 
@@ -28,7 +28,7 @@ console.log('Validador Totémico activo');
       if (!username) {
         errors.username = 'Por favor ingrese un nombre de usuario';
       } else if (!reUsername.test(username)) {
-        errors.username = 'El nombre de usuario debe tener entre 3 y 30 caracteres y puede incluir números o guiones bajos';
+        errors.username = 'Debe tener entre 3 y 30 caracteres y puede incluir números o guiones bajos';
       }
 
       // Nombre
@@ -56,7 +56,7 @@ console.log('Validador Totémico activo');
       if (!pw1) {
         errors.password1 = 'Por favor ingrese una contraseña';
       } else if (!rePw.test(pw1)) {
-        errors.password1 = 'La contraseña debe tener 6–18 caracteres, una mayúscula y un número';
+        errors.password1 = 'Debe tener 6–18 caracteres, una mayúscula y un número';
       }
 
       // Confirmar contraseña
@@ -66,31 +66,38 @@ console.log('Validador Totémico activo');
         errors.password2 = 'Las contraseñas no coinciden';
       }
 
-      // Dirección: 3–40 caracteres + dígito
+      // Dirección
       if (!direccion) {
         errors.direccion = 'Por favor ingrese su dirección';
       } else if (direccion.length < 3 || direccion.length > 40) {
-        errors.direccion = 'La dirección debe tener entre 3 y 40 caracteres';
+        errors.direccion = 'Debe tener entre 3 y 40 caracteres';
       } else if (!/\d+/.test(direccion)) {
-        errors.direccion = 'La dirección debe incluir numeración obligatoria';
+        errors.direccion = 'Debe incluir numeración obligatoria';
       }
 
-      // Comuna 3–30 caracteres
+      // Número de Dpto/Casa
+      if (!numero_dpto_casa) {
+        errors.numero_dpto_casa = 'Por favor ingrese el número de Dpto/Casa';
+      } else if (numero_dpto_casa.length > 10) {
+        errors.numero_dpto_casa = 'Máximo 10 caracteres permitidos';
+      }
+
+      // Comuna
       if (!comuna) {
         errors.comuna = 'Por favor ingrese su comuna';
       } else if (!reLetters.test(comuna)) {
-        errors.comuna = 'La comuna debe tener entre 3 y 30 letras';
+        errors.comuna = 'Debe tener entre 3 y 30 letras';
       }
 
-      // Región 3–30 caracteres
+      // Región
       if (!region) {
         errors.region = 'Por favor ingrese su región';
       } else if (!reLetters.test(region)) {
-        errors.region = 'La región debe tener entre 3 y 30 letras';
+        errors.region = 'Debe tener entre 3 y 30 letras';
       }
 
       // Limpia errores previos
-      $('#username-error, #nombre-error, #apellido-error, #email-error, #password1-error, #password2-error, #direccion-error, #comuna-error, #region-error')
+      $('#username-error, #nombre-error, #apellido-error, #email-error, #password1-error, #password2-error, #direccion-error, #numero_dpto_casa-error, #comuna-error, #region-error')
         .text('');
 
       // Muestra errores inline
